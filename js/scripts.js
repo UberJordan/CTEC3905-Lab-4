9d146382766b3c60ef7917935561ce313914b097
Jordan Burgess <P15184751@LEC-ADMIN.dmu.ac.uk>
2018-10-31 10:10:24 +0000

    commit of completed lab 4 work

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,6 @@
-console.log("hello");
+const menuToggle = document.getElementById('menu-toggle');
+const menuNav = document.getElementById('menu-nav');
+
+const toggleMenu = _ => menuNav.classList.toggle("menu-toggle");
+
+menuToggle.addEventListener("click", toggleMenu);
